c07ea4ff5981ed17833613b6f4f6237d2db1e3c3
nalnahdi <71013411+nalnahdi@users.noreply.github.com>
2020-10-26 21:32:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,37 @@
 
 
 function getRandomColor() {
-  var colors = ['red', 'green', 'blue', 'Orange', 'yellow', 'Light blue','White','Gray','Pink'];
-  var index=Math.floor(Math.random() * 9);
-  var color= colors[index];
+  var colors = ['red', 'green', 'blue', 'Orange', 'yellow', 'Light blue','coral','lightgreen','Pink'];
+  var index;
+  var color;
 
-document.querySelector("div").setAttribute("background-color", color);
+var x =document.querySelectorAll("div");
+var y=document.querySelectorAll("h1");
+//var attributeV= ("background-color,"+ color);
+//x[0].style.backgroundColor = "#AA0000";
+//x[0].style.backgroundColor = color;
+var i;
+for (i = 0; i < x.length; i++) {
+  index=Math.floor(Math.random() * 9);
+  color= colors[index];
+  x[i].style.backgroundColor = color;
+  y[i].innerHTML=Math.floor(Math.random() * 9)+1;
+}
+
+
+
+
+
+
+
+
+
+
+/*x.setAttribute("background-color",color);*/
+ /*document.getElementsByClassName("d1").style.backgroundColor=color;*/
+// document.getElementsByClassName('.d1').backgroundColor = "#00000";
+//"style", "background-color: black;"
+ //document.body.style.backgroundColor = "#AA0000";
 
 }
 
---
+++ main2.js
@@ -0,0 +1,9 @@
+var output= [];
+
+
+document.document.querySelector('HH1').onmouseover = function() { document.getElementById('HH1').style.visibility = "hidden"; };
+output.push(document.getElementById("HH1").innerHTML);
+if(output.length==9)
+{
+  alert('done');
+}
